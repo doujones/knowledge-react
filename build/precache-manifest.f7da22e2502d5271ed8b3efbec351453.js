be55fb93340a90f97170676514732c41aa057fb1
Dou Jones <MISTERCHIEFEXECUTIVEOFFICER@YAHOO.COM>
2020-06-06 13:33:31 -0400

    Add files via upload
    Npm run build was added...

---
+++ build/precache-manifest.f7da22e2502d5271ed8b3efbec351453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e30ee2acf42fbaef4666",
+    "url": "/static/js/main.e30ee2ac.chunk.js"
+  },
+  {
+    "revision": "0fc160f7ef47b9d28fc5",
+    "url": "/static/js/1.0fc160f7.chunk.js"
+  },
+  {
+    "revision": "e30ee2acf42fbaef4666",
+    "url": "/static/css/main.0ce57156.chunk.css"
+  },
+  {
+    "revision": "0fc160f7ef47b9d28fc5",
+    "url": "/static/css/1.2b89494f.chunk.css"
+  },
+  {
+    "revision": "d230fd60e7d910037b45e02ff346bbe0",
+    "url": "/index.html"
+  }
+];
